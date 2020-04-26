231c8cec0587111d401069ebd99ddd47a656691b
Addy Osmani <addyosmani@gmail.com>
2020-04-25 21:41:47 -0700

    [infra] Add yarn and eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly',
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+  },
+  'rules': {
+  },
+};
