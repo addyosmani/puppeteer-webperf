3ba1aba3a23c11974745c13a3dc3096c6cf3bcad
Addy Osmani <addyosmani@gmail.com>
2020-04-25 21:42:33 -0700

    [scripts] add network and cpu throttling

---
+++ throttle-network-cpu.js
@@ -0,0 +1,23 @@
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  let browser = await puppeteer.launch();
+  let page = await browser.newPage();
+
+  const client = await page.target().createCDPSession();
+  await client.send('Network.enable');
+  // Simulated network throttling (Slow 3G)
+  await client.send('Network.emulateNetworkConditions', {
+  // Network connectivity is absent
+  'offline': false,
+  // Download speed (bytes/s)
+  'downloadThroughput': 500 * 1024 / 8 * .8,
+  // Upload speed (bytes/s)
+  'uploadThroughput': 500 * 1024 / 8 * .8,
+  // Latency (ms)
+  'latency': 400 * 5
+});
+  await client.send('Emulation.setCPUThrottlingRate', { rate: 4 });
+  await browser.close();
+})();
