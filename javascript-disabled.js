9cc4a6a29346f8c6caf3b6cd1b0bec71dab90b47
Addy Osmani <addyosmani@gmail.com>
2020-04-25 21:44:01 -0700

    [scripts] add js disabled

---
+++ javascript-disabled.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
+
+    page.on('request', request => {
+        if (request.resourceType() === 'script') {
+            request.abort();
+        } else {
+            request.continue();
+        }
+    });
+
+    await page.goto('https://reddit.com');
+    await page.screenshot({ path: 'pptr-nojs.png' });
+
+    await browser.close();
+})();
