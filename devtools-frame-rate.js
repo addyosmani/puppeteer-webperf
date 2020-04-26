7993b4f4f1f72719fd3baec8a65c4692418bd5e8
Addy Osmani <addyosmani@gmail.com>
2020-04-25 21:45:08 -0700

    [scripts] add fps devtools

---
+++ devtools-frame-rate.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const args = await puppeteer.defaultArgs().filter(flag => flag !== '--enable-automation');
+    const browser = await puppeteer.launch({
+        headless: false,
+        devtools: true,
+        ignoreDefaultArgs: true,
+        args
+    });
+    const page = await browser.newPage();
+    const devtoolsProtocolClient = await page.target().createCDPSession();
+    await devtoolsProtocolClient.send('Overlay.setShowFPSCounter', { show: true });
+    await page.goto('https://pptr.dev');
+    await page.screenshot({ path: './image.jpg', type: 'jpeg' });
+    await page.close();
+    await browser.close();
+})();
