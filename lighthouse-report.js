82413822fe5956bc44e0bb46e22d76524f0c6611
Addy Osmani <addyosmani@gmail.com>
2020-04-25 21:43:38 -0700

    [scripts] add lighthouse report

---
+++ lighthouse-report.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const lighthouse = require('lighthouse');
+const puppeteer = require('puppeteer');
+
+const chromeLauncher = require('chrome-launcher');
+const reportGenerator = require('lighthouse/lighthouse-core/report/report-generator');
+const request = require('request');
+const util = require('util');
+
+const options = {
+  logLevel: 'info',
+  disableDeviceEmulation: true,
+  chromeFlags: ['--disable-mobile-emulation']
+};
+
+async function lighthouseFromPuppeteer(url, options, config = null) {
+  // Launch chrome using chrome-launcher
+  const chrome = await chromeLauncher.launch(options);
+  options.port = chrome.port;
+
+  // Connect chrome-launcher to puppeteer
+  const resp = await util.promisify(request)(`http://localhost:${options.port}/json/version`);
+  const { webSocketDebuggerUrl } = JSON.parse(resp.body);
+  const browser = await puppeteer.connect({ browserWSEndpoint: webSocketDebuggerUrl });
+
+  // Run Lighthouse
+  const { lhr } = await lighthouse(url, options, config);
+  await browser.disconnect();
+  await chrome.kill();
+
+  const html = reportGenerator.generateReport(lhr, 'html');
+  fs.writeFile('report.html', html, function (err) {
+    if (err) throw err;
+  });
+}
+
+lighthouseFromPuppeteer("https://pptr.dev", options);
